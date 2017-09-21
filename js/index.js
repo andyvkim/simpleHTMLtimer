cdfe6e7fbfe13ab749a9eb4cd52708979a0b7cb4
Jhum0407 <jhum0407@yahoo.com>
2017-09-21 11:04:12 -0400

    animations added

--- js/index.js
+++ js/index.js
@@ -1,58 +1,62 @@
+window.onload = function() {
+var heartbeat= document.getElementById('heartbeat');
+
 document.getElementById('timer').innerHTML =
-  01 + ":" + 00;
-  var heartbeat = document.getElementById("heartbeat");
+00 + ":" + 35;
+heartbeat.play();
+startTimer();
 
 function startTimer() {
-
-  var presentTime = document.getElementById('timer').innerHTML;
-  var timeArray = presentTime.split(/[:]+/);
-  var m = timeArray[0];
-  if(m>0){
+var presentTime = document.getElementById('timer').innerHTML;
+var timeArray = presentTime.split(/[:]+/);
+var m = timeArray[0];
+if(m>0){
   var s = checkSecond((timeArray[1] - 1));
   if(s==59){m=m-1}
   }
   else{
     var s = checkSecondLastMinute((timeArray[1] - 1));
-    if(s<=16 && s>0){
+    if(s<=10 && s>0){
       var s = checkSecondLastMinute((timeArray[1] - 1));
-     heartbeat.play();
-     console.log(heartbeat.src);
-     
+      
+    }
+    if(s==0){
+      s="00";
+      document.getElementById('timer').innerHTML =
+      m + ":" + s;
     }
     if (s<=0){
-      heartbeat.pause();
-      var s = checkSecondLastMinute((timeArray[1] - 1));
+      s="00";
+      return;
     }
   }
-  //if(m<0){alert('timer completed')}
-  
-  document.getElementById('timer').innerHTML =
-    m + ":" + s;
-  setTimeout(startTimer, 1000);
+//if(m<0){alert('timer completed')}
+
+
+document.getElementById('timer').innerHTML =
+  m + ":" + s;
+  if(m==0&&s<=10){ 
+    document.getElementById('timer').style.color="red";
+  }
+setTimeout(startTimer, 1000);
 }
 
 function checkSecond(sec) {
-  if (sec < 10 && sec >= 0) {sec = "0" + sec}; // add zero in front of numbers < 10
-  if (sec < 0) {sec = "59"};
-  return sec;
+if (sec < 10 && sec >= 0) {sec = "0" + sec}; // add zero in front of numbers < 10
+if (sec < 0) {sec = "59"};
+return sec;
 }
-
 function checkSecondLastMinute(sec){
   if (sec < 10 && sec >= 0) {sec = "0" + sec};
   if(sec == 0){
-    heartbeat.setAttribute('src','assets/buzzer.mp3');
-    console.log(heartbeat.src);
-    heartbeat.play();
+   sec="00";
   }
   if(sec<0){
-    sec="00";
+    return;
+    
   }
    // add zero in front of numbers < 10
   
-  
   return sec;
 }
-function resetTimer(){
-  document.getElementById('timer').innerHTML =
-  01 + ":" + 00;
 }
